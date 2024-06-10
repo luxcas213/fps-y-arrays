80bb6c39071b20add5c334ac9759c199ef3923a0
GARBATE LUCAS <48521900@ort>
2024-06-10 15:34:17 -0300

    trabajo completo

---
+++ Assets/hambresqript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hambresqript : MonoBehaviour
+{
+    public int si=10;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<alimento>())
+        {
+
+            alimento alimento = other.GetComponent<alimento>();
+            si -= alimento.valorcomida;
+            Destroy(other.gameObject);
+            Debug.Log(si);
+        }
+    }
+}
